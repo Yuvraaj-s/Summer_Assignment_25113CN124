56b91373ef4b407814513baefce4049c1168431c
Yuvraaj-s <yuvraajsrivastava40@gmail.com>
2026-06-27 02:32:51 +0530

    Day-10 uploaded

--- Day 11/ques41.py
+++
@@ -1,9 +0,0 @@
-# Write a program to Write function to find sum of two numbers. 
-
-def add(a, b):
-    return a + b
-
-x = int(input("Enter first number: "))
-y = int(input("Enter second number: "))
-
-print("Sum =", add(x, y))
